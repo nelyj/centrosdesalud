e7071c12fdac713d05e5a383e3ae9332cd91d6e3
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-09-25 14:09:40 -0300

    Agrego cambios geocode after_validation

--- app/models/centro.rb
+++ app/models/centro.rb
@@ -1,6 +1,6 @@
 class Centro < ActiveRecord::Base
 
   geocoded_by :direccion
-  after_validation :geocode, :if => :direccion_changed?
+  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
   
 end
