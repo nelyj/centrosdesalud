8269549b2bd4c637e1a8146695a8169b3bb451e1
Felipe Valdivia <fe.valdiviaa@gmail.com>
2014-09-25 14:57:02 -0300

    se creo un archivo app.js y se realizo la conguracion de angular

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,9 @@
+//= require angular
+//= require angular-animate
+//= require angular-resource
+//= require angular-route
+//= require angular ui-router
+//= require angularUiBootstrap
+//= require_tree ./angular/models
+//= require_tree ./angular/controllers
+
