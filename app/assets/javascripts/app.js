f90447e82989beab03860483e33946704acb5a49
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-09-25 22:19:03 -0300

    Subo los cambios realizados

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,10 +2,14 @@
 //= require angular-animate
 //= require angular-resource
 //= require angular-route
+//= require angular-touch
+
+//= require angular-google-maps.min
+
 //= require_tree ./angular/models
 //= require_tree ./angular/controllers
 
-app = angular.module('Centros', ['ngRoute','ngAnimate','ngResource', 'appCtrl']);
+app = angular.module('Centros', ['ngRoute','ngAnimate','ngResource', 'google-maps', 'ngTouch', 'appCtrl']);
 
 app.config(['$locationProvider', '$httpProvider', '$routeProvider',
 function($locationProvider, $httpProvider, $routeProvider){
