bae340d8f58b73e19cd289ffe6530523899e222d
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-09-26 08:31:45 -0300

    Agrego cambios en application.rb para agregar los fonts y templates

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Centrosdesalud
     # config.i18n.default_locale = :de
 
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts", "templates")
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
 
