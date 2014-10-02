9433c5915462c17d614946d0e6b227cabfe060a7
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-10-02 17:58:05 -0300

    Agrego google maps, pero aun estoy haciendo un par de pruebas para ver como funciona el mapa. Google maps esta funcionando con bower

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,10 @@
 //= require angular/angular
 //= require angular-route/angular-route
 //= require angular-rails-templates
+//= require lodash/dist/lodash
+//= require angular-google-maps/dist/angular-google-maps
 
-app = angular.module('Centros',['templates','ngRoute', 'Controllers']);
+app = angular.module('Centros',['templates','ngRoute', 'google-maps', 'Controllers']);
 
 app.config(['$routeProvider',function($routeProvider){
 	$routeProvider
--- app/assets/javascripts/controllers/homeCtrl.js
+++ app/assets/javascripts/controllers/homeCtrl.js
@@ -1,5 +1,24 @@
 app = angular.module('Controllers', []);
 
+function initCall() {
+  console.log('Google maps api initialized.');
+  angular.bootstrap(document.getElementById('map'), ['doc.ui-map']);
+}
+
 app.controller('homeCtrl',['$scope','$routeParams',function($scope, $routeParams){
-	$scope.hola = "Hola Mundo, Felipe";
+	$scope.background="home";
+
+
+  $scope.map = {
+    center: {
+        latitude: -33.4262800,
+        longitude: -70.5665600
+    },
+    options:{
+      animation: google.maps.Animation.BOUNCE
+    },
+    zoom: 14
+  };
+
+
 }]);
